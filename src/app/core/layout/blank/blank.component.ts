2560ea68a6301135702b298f28f3c3ddf15bd2ae
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-02-22 11:40:45 +0200

    ADD Footer

--- src/app/core/layout/blank/blank.component.ts
+++ src/app/core/layout/blank/blank.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-blank',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.scss'
 })
