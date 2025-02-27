3709a553bffd4a0beb6c7674d0b0f3abef956ef4
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-02-27 19:57:51 +0200

    ADD btn Scroll Top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from './shared/services/flowbite/flowbite.service';
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { BtnScrollTopComponent } from "./core/components/btn-scroll-top/btn-scroll-top.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSpinnerComponent],
+  imports: [RouterOutlet, NgxSpinnerComponent, BtnScrollTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
